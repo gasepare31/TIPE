1c58e916ab6cbd8e9af8350e38c1b664e713cf7e
gaspard <henault.gaspard.tls@gmail.com>
2026-05-16 11:38:23 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("Monuments_avec_voisins.csv")
+
+plt.scatter(df['Longitude'], df['Latitude'], c='red', marker='o', alpha=0.6, edgecolors='black')
+
+plt.title('Répartition spatiale des Monuments')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.show()
+#plt.grid(True, linestyle='--', alpha=0.5) 
+
+#plt.savefig('carte_monuments.png')
